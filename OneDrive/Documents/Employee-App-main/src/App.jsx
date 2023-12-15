1e13e468c8a97d8d34b97bf0eba81ef34bc85046
Brianna <briannaa.kennedy@gmail.com>
2023-12-15 17:18:50 -0500

    updated homework

---
+++ OneDrive/Documents/Employee-App-main/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Employee from "./pages/Employee";
+import Homepage from "./pages/Homepage"
+
+
+function App() {
+  
+  return (
+          <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/employee/:first_name" element={<Employee />} />
+      </Routes>
+   
+  );
+}
+
+export default App;
